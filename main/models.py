9e52091c37d6abca149d56503d3c3454a30c568a
amasdeogratias <deogratiasprog@gmail.com>
2022-07-22 17:23:54 +0300

    added product model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
+from email.headerregistry import Group
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    ProductId = models.CharField(max_length=200)
+    Name = models.CharField(max_length=200, null=True)
+    Code = models.CharField(max_length=200, null=True)
+    Group = models.CharField(max_length=200, null=True)
+    MainGroup = models.CharField(max_length=200, null=True)
+    Unit = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.Name
+        
+    
