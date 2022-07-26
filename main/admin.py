8d788c54a1c8054c361cbffe3bcd5f0c25554905
amasdeogratias <deogratiasprog@gmail.com>
2022-07-26 11:48:05 +0300

    created smarket super user

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
+from gettext import Catalog
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Catalog)
--- main/models.py
+++ main/models.py
@@ -5,12 +5,16 @@ from django.db import models
 class Product(models.Model):
     ProductId = models.CharField(max_length=200)
     Name = models.CharField(max_length=200, null=True)
+    Price = models.DecimalField(max_digits=7, decimal_places=2)
     Code = models.CharField(max_length=200, null=True)
     Group = models.CharField(max_length=200, null=True)
     MainGroup = models.CharField(max_length=200, null=True)
     Unit = models.CharField(max_length=200, null=True)
+    image = models.ImageField(null=True, blank=True)
     
-    def __str__(self):
+    def __str__(self): 
         return self.Name
+    
+
         
     
