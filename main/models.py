b40d71723ad57ecfa1311c8de07048a939fa597a
amasdeogratias <deogratiasprog@gmail.com>
2022-07-26 12:41:04 +0300

    changes to product model

--- main/models.py
+++ main/models.py
@@ -3,14 +3,13 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    ProductId = models.CharField(max_length=200)
     Name = models.CharField(max_length=200, null=True)
-    Price = models.DecimalField(max_digits=7, decimal_places=2)
+    Price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
     Code = models.CharField(max_length=200, null=True)
     Group = models.CharField(max_length=200, null=True)
     MainGroup = models.CharField(max_length=200, null=True)
     Unit = models.CharField(max_length=200, null=True)
-    image = models.ImageField(null=True, blank=True)
+    Image = models.ImageField(null=True, blank=True)
     
     def __str__(self): 
         return self.Name
