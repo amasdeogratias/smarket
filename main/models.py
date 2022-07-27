c130dc13bd440ad435c56c4ee2e7d286d2732cf1
amasdeogratias <deogratiasprog@gmail.com>
2022-07-27 15:57:33 +0300

    added relationship for catalog and product models

--- main/models.py
+++ main/models.py
@@ -1,5 +1,7 @@
 from email.headerregistry import Group
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
@@ -9,10 +11,30 @@ class Product(models.Model):
     Group = models.CharField(max_length=200, null=True)
     MainGroup = models.CharField(max_length=200, null=True)
     Unit = models.CharField(max_length=200, null=True)
-    Image = models.ImageField(null=True, blank=True)
+    
     
     def __str__(self): 
         return self.Name
+
+class Catalog(models.Model):
+    product = models.OneToOneField(Product, null=True, blank=True, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    description = models.TextField(null=True)
+    user =models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.product
+        
+    @property
+    def imageUrl(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+        
+        
+    
     
 
         
